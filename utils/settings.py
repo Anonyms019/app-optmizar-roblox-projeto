a7373fc00dea53183ac5c1adad42bbe4ebb0ff6d
Anonyms019 <ksla57352@gmail.com>
2026-02-23 05:57:24 -0500

    Create settings.py

---
+++ utils/settings.py
@@ -0,0 +1,7 @@
+def optimize_graphics():
+    print("Aplicando configurações gráficas otimizadas...")
+    # Aqui você poderia criar perfis para:
+    # - Reduzir resolução
+    # - Desligar sombras
+    # - Reduzir efeitos visuais
+    print("Configurações aplicadas!")
